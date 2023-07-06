b578f5c3b62ada63d5d8f0c286fb621ea4678478
Harshs2512 <harshsoni1234ji@gmail.com>
2023-07-06 14:01:03 +0530

    vercel

--- frontend/src/pages/Auth/register.js
+++ frontend/src/pages/Auth/register.js
@@ -15,7 +15,7 @@ export default function SignUp() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post("https://tshirts-hub.vercel.app/api/v1/register", {
+            const res = await axios.post("/api/v1/register", {
                 email,
                 password,
                 fname,
